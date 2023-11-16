9e19691c326edbbe8723fc141bfeab8fed4ba698
Zonia Palacios Zorrilla <palacioszorrilla@gmail.com>
2023-11-15 22:10:15 -0300

    ultimaclase

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -31,15 +31,17 @@ const Api = props =>{
         <div className="personajes">
 
             {personajes.map(personaje =>( 
-            <div className="personaje">
-                <h4>Nmbre del personaje </h4>
+            <div className="personaje" key={personaje.id}>
+
+                <h4>{personaje.name} </h4>
                 <div className="ficha">
                     <div className="foto">
-                        <img src="https://fakeimg.pl/100x100" alt="" />
+                        <img src={personaje.image} alt={personaje.name} />
                     </div>
                         <div className="datos">
-                        <h6>Especie: Humano o No </h6>
-                        <h6>Vivo: Si o No </h6>
+                        {/*si personaje es === imprimime  humano si no otro especie */}    
+                        <h6>Especie: {personaje.species === 'Human' ? 'Humano': personaje.species} </h6>
+                        <h6>Vivo:{personaje.status === 'Alive' ? 'Si': 'No'} </h6>
 
                         </div>
                 </div>
